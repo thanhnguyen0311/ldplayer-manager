a694ef1e3f4be91ff9a89a863bc742c51899f143
DESKTOP-4JH7URA\Thanh <nct031194@icloud.com>
2023-12-18 00:13:41 +0700

    + add scrollbar

--- views/layouts/MainFrame.py
+++ views/layouts/MainFrame.py
@@ -4,17 +4,35 @@ from views.pages.LDManager import LDManager_Page
 
 
 class MainFrame(tk.Frame):
-    def __init__(self,master):
-        tk.Frame.__init__(self, master)
+    def __init__(self, master, *args, **kwargs):
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.scrollbar = tk.Scrollbar(self)
+        self.canvas_scrollbar_pairs = []
+        self.canvas = tk.Canvas(self)
         self.Frame = None
-        self.configure(width=650)
         self.choose_page(LDManager_Page)
 
     def delete_pages(self):
+        children = list(self.children.values())
+        for child in children:
+            child.destroy()
+
         for frame in self.winfo_children():
             frame.destroy()
 
     def choose_page(self, page):
         self.delete_pages()
-        self.Frame = page(self)
-        self.Frame.pack()
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.Frame = page(self.canvas)
+        self.canvas.create_window((0, 0), window=self.Frame, anchor="nw")
+        self.scrollbar = tk.Scrollbar(self)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scrollbar.config(command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind("<Configure>", self.on_configure)
+        self.canvas_scrollbar_pairs.append([self.canvas, self.scrollbar])
+
+    def on_configure(self, event):
+        # Update the scroll region to encompass the entire canvas
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
--- views/pages/LDManager.py
+++ views/pages/LDManager.py
@@ -1,4 +1,7 @@
-import time
+from threading import Thread
+from time import sleep
+from queue import Queue
+
 import tkinter as tk
 
 from ld_manager.create_ld import create_ld
@@ -6,13 +9,13 @@ from ld_manager.get_list_ld import get_list_ld
 
 
 class LDManager_Page(tk.Frame):
-    def __init__(self, master):
-        tk.Frame.__init__(self, master)
+    def __init__(self, master, *args, **kwargs):
+        tk.Frame.__init__(self, master, *args, **kwargs)
         self.data = get_list_ld()
         self.menu_bar = tk.Frame(self)
         self.menu_bar.grid(row=1, column=0, sticky="w", padx=5)
 
-        self.button_add = tk.Button(self.menu_bar, text="Add", width=10, height=2, command=self.add_device)
+        self.button_add = tk.Button(self.menu_bar, text="Add", width=10, height=2, command=lambda: self.add_device(1))
         self.button_add.grid(row=1, column=1, pady=5)
         self.button_get = tk.Button(self.menu_bar, text="Refresh", width=10, height=2, command=self.refresh)
         self.button_get.grid(row=1, column=2, pady=5)
@@ -43,15 +46,21 @@ class LDManager_Page(tk.Frame):
         self.device_frame()
         self.data = get_list_ld()
         for index, i in enumerate(self.data):
-            self.device_line(index + 1, i)
+            self.device_line(i)
 
-    def device_line(self, index, device):
-        tk.Label(self.device_list, text=device.ID).grid(row=device.ID, column=0)
-        tk.Label(self.device_list, text=device.name).grid(row=device.ID, column=1)
-        tk.Label(self.device_list, text=device.imei).grid(row=device.ID, column=2)
-        tk.Label(self.device_list, text=device.uuid).grid(row=device.ID, column=3)
+    def device_line(self, device):
+        row = int(device.ID) + 1
+        tk.Label(self.device_list, text=device.ID).grid(row=row, column=0)
+        tk.Label(self.device_list, text=device.name).grid(row=row, column=1)
+        tk.Label(self.device_list, text=device.imei).grid(row=row, column=2)
+        tk.Label(self.device_list, text=device.uuid).grid(row=row, column=3)
 
-    def add_device(self):
-        device = create_ld(1)
-        self.refresh()
+    def add_device(self, number):
+        thread = Thread(target=self.threaded_function, args=(number,))
+        thread.start()
+
+    def threaded_function(self, arg):
+        for i in range(arg):
+            create_ld(1)
+            self.refresh()
 
